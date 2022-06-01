a2822e4409a205458bf00293f322bc6c21f7d84b
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-01 15:14:19 +0300

    new

---
+++ task1-react-intro/1-first-react-app/index.js
@@ -0,0 +1,9 @@
+const rootElem = document.querySelector("#root");
+
+const greetingElem = React.createElement(
+  "div",
+  { className: "greeting" },
+  "Hello, React!"
+);
+
+ReactDOM.render(greetingElem, rootElem);

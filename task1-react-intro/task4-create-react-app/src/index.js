05d9865e1bb544c2195f16ef4023ba1d558501d0
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-02 16:21:42 +0300

    new

---
+++ task1-react-intro/task4-create-react-app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const rootElement = document.querySelector("#root");
+
+const element = (
+  <div className="greeting">
+    <div className="greeting__title">Hello, world!</div>
+    <div className="greeting__text">I'm learning React</div>
+  </div>
+);
+
+ReactDOM.render(element, rootElement);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

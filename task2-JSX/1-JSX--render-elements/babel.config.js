f72bbdc3578c4ae9ff1299729a9f2625feb79679
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-07 08:19:52 +0300

    new

---
+++ task2-JSX/1-JSX--render-elements/babel.config.js
@@ -0,0 +1,14 @@
+module.exports ={
+  "presets": [
+    [
+      "@babel/preset-env",
+      {
+        "targets": "> 1%",
+        "useBuiltIns": "usage",
+        "corejs": 3
+      }
+    ],
+    "@babel/preset-react"
+  ]
+}
+;

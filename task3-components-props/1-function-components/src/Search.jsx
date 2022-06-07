da5d54b465a145ecf42aaffefee8245e5786c454
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-07 13:44:41 +0300

    new

--- task3-components-props/1-function-components/src/Search.jsx
+++ task3-components-props/1-function-components/src/Search.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
-const Greeting = () => {
+const Greeting = props => {
   const element = (
     <div className="search">
-      <h1 className="search__title">Hello, Tom. What to search for you?</h1>
+      <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
       <div className="search__field">
         <input type="text" className="search__input" />
         <button className="search__button">Search</button>

112f6210d64db0721158e2c3947f03c43c9eb9cd
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-08 07:54:27 +0300

    new

--- task3-components-props/2-classes-components/babel.config.js
+++ task3-components-props/2-classes-components/babel.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: '> 1%', useBuiltIns: 'usage', corejs: 3 }],
+    [
+      '@babel/preset-env',
+      {
+        targets: '> 1%',
+        useBuiltIns: 'usage',
+        corejs: 3,
+      },
+    ],
     '@babel/preset-react',
   ],
-  plugins: ['@babel/plugin-proposal-class-properties'],
 };

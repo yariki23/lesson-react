19243673238f664c3a1170546353b1a3c8418582
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-08 10:13:36 +0300

    new

--- task3-components-props/3-Composition-components/src/Avatar.jsx
+++ task3-components-props/3-Composition-components/src/Avatar.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import './avatar.scss';
 
 const Avatar = props => {
-  return <img className="avatar" src={props.user.avatarUrl} alt={props.user.name} />;
+  return (
+    <>
+    <img className="avatar" src={props.user.avatarUrl} alt={props.user.name} />
+      <div className="user-info__name">{props.user.name}</div>
+    </>
+  )
 };
 
 export default Avatar;
--- task3-components-props/3-Composition-components/src/UserInfo.jsx
+++ task3-components-props/3-Composition-components/src/UserInfo.jsx
@@ -6,7 +6,6 @@ const UserInfo = props => {
   return (
     <div className="user-info">
       <Avatar user={props.user} />
-      <div className="user-info__name">{props.user.name}</div>
     </div>
   );
 };

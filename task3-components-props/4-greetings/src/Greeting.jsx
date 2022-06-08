40a79bc7182131349bcd99e1c28a5ffe849c2e33
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-08 13:08:21 +0300

    new

--- task3-components-props/4-greetings/src/Greeting.jsx
+++ task3-components-props/4-greetings/src/Greeting.jsx
@@ -4,8 +4,8 @@ import moment from 'moment';
 const Greeting = props => {
   return (
     <div className="greeting">
-      My name is {props.user.firstName} {props.user.lastName}. I'm{' '}
-      {moment().diff(props.user.birthDate, 'years')} years old
+      My name is {props.firstName} {props.lastName}. I'm{' '}
+      {moment().diff(props.birthDate, 'years')} years old
     </div>
   );
 };

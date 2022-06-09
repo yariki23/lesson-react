cb693cf4cfea40a73ab3a0be7d74421cf8e9b0c6
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-09 16:26:55 +0300

    new

--- task4-state/2-world-time/src/App.jsx
+++ task4-state/2-world-time/src/App.jsx
@@ -4,9 +4,9 @@ import Clock from './Clock.jsx';
 const App = () => {
   return (
     <>
-      <Clock location={'New York'} offset={-5} />
-      <Clock location={'Kyiv'} offset={2} />
-      <Clock location={'London'} offset={0} />
+      <Clock location={'NEW YORK'} offset={-5} />
+      <Clock location={'KYIV'} offset={2} />
+      <Clock location={'LONDON'} offset={0} />
     </>
   );
 };

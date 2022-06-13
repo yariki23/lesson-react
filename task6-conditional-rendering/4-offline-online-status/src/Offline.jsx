f3e43832c9257f4193eda4aa3ca519c66bb5d057
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-13 15:23:52 +0300

    new

---
+++ task6-conditional-rendering/4-offline-online-status/src/Offline.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Offline = props => {
+  return (
+    <>
+      <span className="status__text">Offline</span>
+      <button className="status__btn" onClick={props.reconnectStatus}>
+        Reconnect
+      </button>
+    </>
+  );
+};
+
+export default Offline;
---
+++ task6-conditional-rendering/4-offline-online-status/src/Status.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Online from './Online.jsx';
+import Offline from './Offline.jsx';
+
+class Status extends React.Component {
+  state = {
+    isOnline: false,
+  };
+
+  reconnectStatus = () => {
+    this.setState({
+      isOnline: true,
+    });
+  };
+
+  render() {
+    const checkOnline = this.state.isOnline ? (
+      <Online />
+    ) : (
+      <Offline reconnectStatus={this.reconnectStatus} />
+    );
+    return <div className="status">{checkOnline}</div>;
+  }
+}
+
+export default Status;

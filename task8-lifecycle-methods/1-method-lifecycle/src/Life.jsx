6e78821fa9ab721b9127c922265b9d847c669392
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-20 09:02:36 +0300

    new

--- task8-lifecycle-methods/1-method-lifecycle/src/Life.jsx
+++ task8-lifecycle-methods/1-method-lifecycle/src/Life.jsx
@@ -17,7 +17,7 @@ class Life extends Component {
     // return true;
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate() {
     console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
   }
 

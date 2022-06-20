1cf07c0b4aeaa434af7081a5d4da73dcd40545c7
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-20 08:01:38 +0300

    new

--- task8-lifecycle-methods/1-method-lifecycle/src/Life.jsx
+++ task8-lifecycle-methods/1-method-lifecycle/src/Life.jsx
@@ -3,23 +3,23 @@ import React, { Component } from 'react';
 class Life extends Component {
   constructor(props) {
     super(props);
-    console.log('good palce to create state');
+    console.log('constructor: good place to create state');
   }
 
   componentDidMount() {
-    console.log('API calls, subscriptions');
+    console.log('componentDidMount: API calls, subscriptions');
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log('decide to render or not to render');
+    console.log('shouldComponentUpdate: decide to render or not to render');
   }
 
   componentWillUnmount() {
-    console.log('cleanup before DOM related to component will be removed');
+    console.log('componentWillUnmount: cleanup before DOM related to component will be removed');
   }
 
   render() {
-    console.log('React element to build DOM');
+    console.log('return React element to build DOM');
     return <div className="number">32</div>;
   }
 }

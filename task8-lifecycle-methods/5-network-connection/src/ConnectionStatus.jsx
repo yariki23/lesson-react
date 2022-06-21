12242cc28cf4b97d7bbf529c55c5b431cf862e9c
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-21 11:51:53 +0300

    new

---
+++ task8-lifecycle-methods/5-network-connection/src/ConnectionStatus.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class ConnectionStatus extends React.Component {
+  state = {
+    status: 'online',
+    borderClass: 'status',
+  };
+
+  componentDidMount() {
+    window.addEventListener('offline', this.offline);
+    window.addEventListener('online', this.online);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('offline', this.offline);
+    window.removeEventListener('online', this.online);
+  }
+
+  offline = e => {
+    this.setState({
+      status: 'offline',
+      borderClass: 'status status_offline',
+    });
+  };
+
+  online = e => {
+    this.setState({
+      status: 'online',
+      borderClass: 'status',
+    });
+  };
+
+  toggleClass = () => {
+    window.removeEventListener('offline', () => true);
+  };
+
+  render() {
+    return <div className={this.state.borderClass}>{this.state.status}</div>;
+  }
+}
+
+export default ConnectionStatus;

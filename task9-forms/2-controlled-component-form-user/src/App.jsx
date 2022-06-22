cb9c2569575b54f0b2baab9e1b66a60f5e97ba9f
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-22 13:16:32 +0300

    new

--- task9-forms/2-controlled-component-form-user/src/App.jsx
+++ task9-forms/2-controlled-component-form-user/src/App.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import UserForm from './UserForm.jsx';
 
 class App extends React.Component {
-  createUser = (event, userDate) => {
-    event.preventDefault();
+  createUser = (userDate) => {
     console.log(userDate);
   };
   render() {

477df8feafa171dcb3e33c355401c421889b5d71
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-22 10:43:39 +0300

    new

--- task9-forms/2-controlled-component-form-user/src/UserForm.jsx
+++ task9-forms/2-controlled-component-form-user/src/UserForm.jsx
@@ -26,7 +26,7 @@ class UserForm extends React.Component {
 
   render() {
     return (
-      <form className="login-form" onSubmit={this.handleSubmit()}>
+      <form className="login-form" onSubmit={this.handleSubmit}>
         <h1 className="form-title">Profile</h1>
         <div className="form-control">
           <label className="form-label" htmlFor="name">

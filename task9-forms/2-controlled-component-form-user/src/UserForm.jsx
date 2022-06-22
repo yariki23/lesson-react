178fbe0037f5bf4e584a2a2a46a5eb959787f1dd
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-22 11:39:36 +0300

    new

--- task9-forms/2-controlled-component-form-user/src/UserForm.jsx
+++ task9-forms/2-controlled-component-form-user/src/UserForm.jsx
@@ -23,14 +23,10 @@ class UserForm extends React.Component {
     // console.log(this.state);
     this.props.createUser(this.state);
   };
-  // (e) => {
-  //       e.preventDefault();
-  //       return this.props.createUser(this.state)
-  //     }
 
   render() {
     return (
-      <form className="login-form" onSubmit={this.props.createUser(this.state)}>
+      <form className="login-form" onSubmit={() => this.props.createUser(this.state)}>
         <h1 className="form-title">Profile</h1>
         <div className="form-control">
           <label className="form-label" htmlFor="name">

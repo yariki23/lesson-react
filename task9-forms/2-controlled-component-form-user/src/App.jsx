68fbc9a3008308ccf12e17629481e8dd9c711566
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-22 10:57:26 +0300

    new

--- task9-forms/2-controlled-component-form-user/src/App.jsx
+++ task9-forms/2-controlled-component-form-user/src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import UserForm from './UserForm.jsx';
 
 class App extends React.Component {
-  createUser = objUser => {
-    console.log(objUser);
+  createUser = event => {
+    event.preventDefault();
+    console.log(this.state);
   };
   render() {
     return <UserForm createUser={this.createUser} />;
--- task9-forms/2-controlled-component-form-user/src/UserForm.jsx
+++ task9-forms/2-controlled-component-form-user/src/UserForm.jsx
@@ -26,7 +26,7 @@ class UserForm extends React.Component {
 
   render() {
     return (
-      <form className="login-form" onSubmit={this.onSubmit}>
+      <form className="login-form" onSubmit={() => this.props.createUser}>
         <h1 className="form-title">Profile</h1>
         <div className="form-control">
           <label className="form-label" htmlFor="name">

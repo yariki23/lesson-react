1fb94fef4b8a9e277f3f59ba089a86fc6a4b1b0b
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-23 15:36:37 +0300

    new

--- task9-forms/4-list-with-filter/src/User.jsx
+++ task9-forms/4-list-with-filter/src/User.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const User = props => {
   return (
     <li className="user">
-      <span className="user__name">{props.name}</span>
-      <span className="user__age">{props.age}</span>
+      {/* <span className="user__name">{props.name}</span>
+      <span className="user__age">{props.age}</span> */}
     </li>
   );
 };

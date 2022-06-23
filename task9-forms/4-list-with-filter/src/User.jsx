767a0f45c6aa9a6debd518863f28b148c6dd889d
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-23 15:38:11 +0300

    new

--- task9-forms/4-list-with-filter/src/User.jsx
+++ task9-forms/4-list-with-filter/src/User.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 
 const User = props => {
-  return (
-    <li className="user">
-      {/* <span className="user__name">{props.name}</span>
-      <span className="user__age">{props.age}</span> */}
-    </li>
-  );
+  return <></>;
 };
 
 export default User;

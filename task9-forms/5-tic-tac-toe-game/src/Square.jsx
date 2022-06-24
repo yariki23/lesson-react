9b9540908e52c0d6c3294f6ea7f862d9abc3eecd
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-24 14:26:08 +0300

    new

--- task9-forms/5-tic-tac-toe-game/src/Square.jsx
+++ task9-forms/5-tic-tac-toe-game/src/Square.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-class Square extends React.Component {
-  render() {
-    return <button className="square">{this.props.value}</button>;
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;

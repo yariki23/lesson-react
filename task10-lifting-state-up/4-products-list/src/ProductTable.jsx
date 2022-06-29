a0851b5946b1bb9a66f87ae04fd8f328c5ca4900
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-29 13:41:22 +0300

    new

--- task10-lifting-state-up/4-products-list/src/ProductTable.jsx
+++ task10-lifting-state-up/4-products-list/src/ProductTable.jsx
@@ -5,15 +5,15 @@ import ProductCategoryRow from './FilterableProductTable';
 class ProductTable extends React.Component {
   render() {
     const rows = [];
-    // let lastCategory = null;
+    let lastCategory = null;
 
-    // this.props.products.forEach(product => {
-    //   if (product.category !== lastCategory) {
-    //     rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
-    //   }
-    //   rows.push(<ProductRow product={product} key={product.name} />);
-    //   lastCategory = product.category;
-    // });
+    this.props.products.forEach(product => {
+      if (product.category !== lastCategory) {
+        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+      }
+      rows.push(<ProductRow product={product} key={product.name} />);
+      lastCategory = product.category;
+    });
 
     return (
       <table>

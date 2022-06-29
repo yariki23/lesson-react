e0ad75de3e34e54a81b585e00de2e065c09dc6f3
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-29 15:29:56 +0300

    new

--- task10-lifting-state-up/4-products-list/src/FilterableProductTable.jsx
+++ task10-lifting-state-up/4-products-list/src/FilterableProductTable.jsx
@@ -3,11 +3,43 @@ import SearchBar from './SearchBar.jsx';
 import ProductTable from './ProductTable.jsx';
 
 class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false,
+    };
+
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockChange = this.handleInStockChange.bind(this);
+  }
+
+  handleFilterTextChange(filterText) {
+    this.setState({
+      filterText: filterText,
+    });
+  }
+
+  handleInStockChange(inStockOnly) {
+    this.setState({
+      inStockOnly: inStockOnly,
+    });
+  }
+
   render() {
     return (
       <div>
-        <SearchBar />
-        <ProductTable products={this.props.products} />
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          onFilterTextChange={this.handleFilterTextChange}
+          onInStockChange={this.handleInStockChange}
+        />
+        <ProductTable
+          products={this.props.products}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
       </div>
     );
   }
--- task10-lifting-state-up/4-products-list/src/ProductTable.jsx
+++ task10-lifting-state-up/4-products-list/src/ProductTable.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import ProductRow from './ProductRow';
-import ProductCategoryRow from './FilterableProductTable';
+import ProductRow from './ProductRow.jsx';
+import ProductCategoryRow from './ProductCategoryRow.jsx';
 
 class ProductTable extends React.Component {
   render() {
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
+
     const rows = [];
     let lastCategory = null;
+    // let prods = this.props.products;
+    // const products = [
+    //   { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
+    //   { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
+    //   { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
+    //   { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
+    //   { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
+    //   { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' },
+    // ];
+    console.log(this.props.products);
 
     this.props.products.forEach(product => {
+      if (product.name.indexOf(filterText) === -1) {
+        return;
+      }
+      if (inStockOnly && !product.stocked) {
+        return;
+      }
       if (product.category !== lastCategory) {
         rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
       }
@@ -28,4 +47,5 @@ class ProductTable extends React.Component {
     );
   }
 }
+
 export default ProductTable;

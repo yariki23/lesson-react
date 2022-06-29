29023b1503de70052d450ddc706adb1da8728e88
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-29 13:26:24 +0300

    new

--- task10-lifting-state-up/4-products-list/src/ProductTable.jsx
+++ task10-lifting-state-up/4-products-list/src/ProductTable.jsx
@@ -5,14 +5,15 @@ import ProductCategoryRow from './FilterableProductTable';
 class ProductTable extends React.Component {
   render() {
     const rows = [];
-    let lastCategory = null;
-    this.props.products.forEach(product => {
-      if (product.category !== lastCategory) {
-        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
-      }
-      rows.push(<ProductRow product={product} key={product.name} />);
-      lastCategory = product.category;
-    });
+    // let lastCategory = null;
+
+    // this.props.products.forEach(product => {
+    //   if (product.category !== lastCategory) {
+    //     rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+    //   }
+    //   rows.push(<ProductRow product={product} key={product.name} />);
+    //   lastCategory = product.category;
+    // });
 
     return (
       <table>
@@ -27,5 +28,4 @@ class ProductTable extends React.Component {
     );
   }
 }
-
 export default ProductTable;
--- task10-lifting-state-up/4-products-list/src/index.jsx
+++ task10-lifting-state-up/4-products-list/src/index.jsx
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import FilterableProductTable from './FilterableProductTable.jsx';
 
-const products = [
-  { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
-  { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
-  { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
-  { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
-  { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
-  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' },
-];
-
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<FilterableProductTable products={products} />, rootElement);
+ReactDOM.render(<FilterableProductTable />, rootElement);

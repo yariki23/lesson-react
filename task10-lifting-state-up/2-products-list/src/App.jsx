616c523e4348e13e005564175d39ba1caee25650
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-29 07:46:39 +0300

    new

--- task10-lifting-state-up/2-products-list/src/App.jsx
+++ task10-lifting-state-up/2-products-list/src/App.jsx
@@ -26,7 +26,7 @@ class App extends Component {
       <div className="page">
         <h1 className="title">{`Hello, ${userData.firstName} ${userData.lastName}`}</h1>
         <main className="content">
-          <ShoppingCart userName={userData} />
+          <ShoppingCart userName={userData.firstName} />
           <Profile userData={userData} handleChange={this.handleChange} />
         </main>
       </div>
--- task10-lifting-state-up/2-products-list/src/ShoppingCart.jsx
+++ task10-lifting-state-up/2-products-list/src/ShoppingCart.jsx
@@ -20,7 +20,7 @@ class ShoppingCart extends Component {
 
   render() {
     const count = this.state.cartItems.length;
-    const userName = this.props.userName.firstName;
+    const userName = this.props.userName;
     return (
       <div className="column">
         <CartTitle userName={userName} count={count} />

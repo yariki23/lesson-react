f6843d45a081a9548cfb51742eb7c73123526608
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-06-29 09:19:35 +0300

    new

--- task10-lifting-state-up/3-Water-temperature-calculator/src/BoilingVerdict.jsx
+++ task10-lifting-state-up/3-Water-temperature-calculator/src/BoilingVerdict.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function BoilingVerdict(props) {
   if (props.celsius >= 100) {
-    return <p>Вода закипит.</p>;
+    return <p>The water would boil.</p>;
   }
-  return <p>Вода не закипит.</p>;
+  return <p>The water would not boil.</p>;
 }
 
 export default BoilingVerdict;

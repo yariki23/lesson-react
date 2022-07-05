366104331d88237ed5fec5754958d29794b7d23d
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-07-05 13:55:56 +0300

    new

--- task14-hooks/2-users-data-react-hooks/src/App.jsx
+++ task14-hooks/2-users-data-react-hooks/src/App.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import User from './User';
 
 const App = () => {
-  return <div>Hello, React!</div>;
+  return (
+    <div className="page">
+      <Router>
+        <div className="page__content">
+          <h1>Users</h1>
+          <ul className="navigation">
+            <li className="navigation__item">
+              <Link to="/users/github">Github</Link>
+            </li>
+            <li className="navigation__item">
+              <Link to="/users/facebook">Facebook</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/users/:userId" component={User} />
+            <Route path="">
+              <span>Select a user please</span>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ task14-hooks/2-users-data-react-hooks/src/User.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+const User = ({ match }) => {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${match.params.userId}`)
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+
+        throw new Error();
+      })
+      .then(userData => {
+        setUserData(userData);
+      });
+  }, [match.params.userId]);
+
+  if (!userData) {
+    return null;
+  }
+  const { name, location, avatar_url } = userData;
+  return (
+    <div className="user">
+      <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+      <div className="user__info">
+        <span className="user__name">{name}</span>
+        <span className="user__location">{location}</span>
+      </div>
+    </div>
+  );
+};
+
+export default User;
